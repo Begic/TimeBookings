a80888f809eb1e7a8b4e5f02ec1a8d80b5465ec2
Süleyman Begic <begic@software.tirol>
2024-01-10 12:00:32 +0100

    add validation

--- TimeBooking.UI/Validation/EditTimeBookingDayValidator.cs
+++ TimeBooking.UI/Validation/EditTimeBookingDayValidator.cs
@@ -1,8 +1,12 @@
-﻿using TimeBooking.Db.Models;
+﻿using FluentValidation;
+using TimeBooking.Db.Models;
 
 namespace TimeBooking.UI.Validation;
 
 public class EditTimeBookingDayValidator : AbstractValidator<EditTimeBookingDay>
 {
-    
+    public EditTimeBookingDayValidator()
+    {
+        RuleFor(x => x.Remark).NotNull().NotEmpty();
+    }
 }
